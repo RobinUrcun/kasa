7a9a672624dce1a1d524bf3f3320af1be94d9828
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2024-02-25 18:12:13 +0100

    ajustement couleur  lien page erreur

--- src/components/Pages/logement/caroussel/caroussel.jsx
+++ src/components/Pages/logement/caroussel/caroussel.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import React from "react";
 
 import arrow_before from "../../../../assets/form/arrow_before.png";
 import arrow_after from "../../../../assets/form/arrow_after.png";
@@ -24,16 +25,20 @@ function Caroussel({ advertissement }) {
   return (
     <div className="logement_img">
       <div className="logement_img_caroussel">
-        <div
-          onClick={previousPicture}
-          className=" caroussel_arrow arrow_before"
-        >
-          <img src={arrow_before} alt="précedent" />
-        </div>
         <img src={advertissement.pictures[pictureNumber]} alt="logement" />
-        <div onClick={nextPicture} className="caroussel_arrow arrow_after">
-          <img src={arrow_after} alt="suivant" />
-        </div>
+        {advertissement.pictures.length === 1 ? null : (
+          <React.Fragment>
+            <div
+              onClick={previousPicture}
+              className=" caroussel_arrow arrow_before"
+            >
+              <img src={arrow_before} alt="précedent" />
+            </div>
+            <div onClick={nextPicture} className="caroussel_arrow arrow_after">
+              <img src={arrow_after} alt="suivant" />
+            </div>
+          </React.Fragment>
+        )}
         <div className="logement_img_number">
           <p>
             {pictureNumber + 1} / {advertissement.pictures.length}
