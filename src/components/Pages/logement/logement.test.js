510da90bdb5741d45cced6258e5f432cfcd502ec
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2024-03-02 16:16:50 +0100

    modification du ReadMe

---
+++ src/components/Pages/logement/logement.test.js
@@ -0,0 +1,6 @@
+import Logement from "./logement";
+import { render } from "@testing-library/react";
+
+describe("test de l'affichage de la page logement", () => {
+  test("test du render de la page si l'id est trouvé", () => {});
+});
